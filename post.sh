c0b44253afd38de0688a95a21234c2b0d8cb14d9
Wei Chen <smile.v.chen@gmail.com>
2018-07-16 17:40:23 +0900

    add post script & update vnote page

---
+++ post.sh
@@ -0,0 +1,24 @@
+# Thanks to http://themainthread.com/blog/2014/11/jekyll-new-post-script.html
+# Also thanks to https://github.com/jackbaty/baty.net-hugo/blob/master/post.sh
+DATE=`date +%Y-%m-%d`
+DATETIME=`date +%Y-%m-%dT%H:%M:%S-04:00`
+YEAR=`date +%Y`
+TITLE=$1
+FILE=content/post/$YEAR/$DATE-$TITLE.md
+
+shift 2
+
+cat > $FILE <<- EOM
+---
+title: "$TITLE"
+date: "${DATETIME}"
+tags: [""]
+draft: false
+
+comment: false
+toc: false
+contentCopyright: '<a rel="license noopener" href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">CC BY-NC-ND 4.0</a>'
+reward: false
+mathjax: true
+---
+EOM
